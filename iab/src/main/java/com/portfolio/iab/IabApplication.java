21de46232ba53bb61d9ea3941e247a920b32a90d
Baranovsky-Ivan <b94.ivan@gmail.com>
2022-08-02 18:13:49 -0300

    iab base de datos

---
+++ iab/src/main/java/com/portfolio/iab/IabApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.iab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IabApplication.class, args);
+	}
+
+}
---
+++ iab/src/main/java/com/portfolio/iab/Security/Repository/iRolRepository.java
@@ -0,0 +1,13 @@
+
+package com.portfolio.iab.Security.Repository;
+
+import com.portfolio.iab.Security.Entity.Rol;
+import com.portfolio.iab.Security.Enums.RolNombre;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface iRolRepository extends JpaRepository<Rol, Integer>{
+    Optional<Rol> findByRolNombre(RolNombre rolNombre);
+}
